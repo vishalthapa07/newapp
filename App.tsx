622a3ae35a842dc02dcf6f94a64003f440179f13
vishalthapa07 <vishalqubits@gmail.com>
2024-08-26 17:57:34 +0545

    setup style for project 2

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import FlatCards from './components/flatCards';
 
-function App() {
+const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World !!</Text>
-        <Text>Hello World !!</Text>
-        <Text>Hello World !!</Text>
-      </View>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 export default App;
---
+++ components/flatCards.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+  container: {},
+  card: {
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#ef5354',
+  },
+});
