ed56972e79e605a89604c55e32110d050a2a916e
vishalthapa07 <vishalqubits@gmail.com>
2024-08-27 17:07:26 +0545

    create card horizontally in mobile

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/flatCards';
+import ElevatedCards from './components/elevatedCards';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCards />
+        <ElevatedCards />
       </ScrollView>
     </SafeAreaView>
   );
--- components/flatCards.tsx
+++ components/flatCards.tsx
@@ -9,6 +9,15 @@ export default function FlatCards() {
         <View style={[styles.card, styles.cardOne]}>
           <Text>Red</Text>
         </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
       </View>
     </View>
   );
@@ -20,8 +29,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 10,
   },
-  container: {},
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+  },
   card: {
+    flex: 1,
+    justifyContent: 'center', //works from top to bottom
+    alignItems: 'center', //works from left to right
     width: 100,
     height: 100,
     borderRadius: 4,
@@ -29,5 +45,11 @@ const styles = StyleSheet.create({
   },
   cardOne: {
     backgroundColor: '#ef5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50dbb4',
+  },
+  cardThree: {
+    backgroundColor: '#5da3fa',
   },
 });
