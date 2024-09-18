e367855cb47a0dfa80b33acb89a14e54842d8ce3
vishalthapa07 <vishalqubits@gmail.com>
2024-09-18 15:31:33 +0545

    add create password logic

--- App.tsx
+++ App.tsx
@@ -1,24 +1,58 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
-import React from 'react';
-import FlatCards from './components/flatCards';
-import ElevatedCards from './components/elevatedCards';
-import FancyCard from './components/fancyCard';
-import ActionCard from './components/actionCard';
-import ContactList from './components/contactList';
+import React, {useState} from 'react';
+import {number, object} from 'yup';
+
+const PasswordSchema = object().shape({
+  passwordLength: number()
+    .min(4, 'Should be min of 4 characters')
+    .max(16, 'Should be max of 16 characters')
+    .required('Length is required'),
+});
 
 const App = () => {
+  const [password, setPasswprd] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, useNumbers] = useState(false);
+  const [symbols, useSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (upperCase) {
+      characterList += upperCase;
+    }
+    if (lowerCase) {
+      characterList += numbers;
+    }
+    if (symbols) {
+      characterList += symbols;
+    }
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+
+    return result;
+  };
+
+  const resetPasswordState = () => {
+    //
+  };
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <FlatCards />
-        <ActionCard />
-        <ContactList />
-        <ElevatedCards />
-        <FancyCard />
-        <FancyCard />
-        <FancyCard />
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
